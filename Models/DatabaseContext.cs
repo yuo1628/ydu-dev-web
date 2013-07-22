967c8522550322f95ac004a5521016c81f0003e9
yuo1628 <pors37@gmail.com>
2013-07-22 15:01:37 +0800

    #2 新增DbContext

---
+++ Models/DatabaseContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MvcApplication1.Models
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<News> news { set; get; }
+        public DbSet<Category> categories { set; get; }
+        public DbSet<File> files { set; get; }
+        public DbSet<Company> companies { set; get; }
+        public DbSet<Product> products { set; get; }
+        public DbSet<Paragon> paragons { set; get; }
+        public DbSet<Founder> founders { set; get; }
+    }
+}
