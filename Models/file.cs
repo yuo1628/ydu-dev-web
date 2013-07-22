b378baf11741b01b61a11b7b44ffe8e724473724
yuo1628 <pors37@gmail.com>
2013-07-22 14:35:57 +0800

    #2 新增Model類別

---
+++ Models/file.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcApplication1.Models
+{
+    public class File
+    {
+        // Primary Key
+        public int fileId { set; get; }
+
+        // 檔案名稱
+        public string name { set; get; }
+        // 檔案路徑
+        public string path { set; get; }
+
+        // Navigation Property
+        public virtual News news { set; get; }
+        public virtual Product product { set; get; }
+        public virtual Paragon paragon { set; get; }
+    }
+}
---
+++ Models/news.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcApplication1.Models
+{
+    public class News
+    {
+        // Primary Key
+        public int newsId { set; get; }
+
+        public string title { set; get; }
+        public string content { set; get; }
+        public Nullable<DateTime> publish_time;
+        public Nullable<DateTime> edit_time;
+
+        // Foreign Key
+        public int categoryId { set; get; }
+
+        // Navigation Property
+        public virtual Category category { set; get; }
+        public virtual ICollection<File> files { set; get; }
+    }
+}
